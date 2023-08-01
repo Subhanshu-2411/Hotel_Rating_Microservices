c7fa4a76e4384982bc160427c8f0af2da2e197a1
Subhanshu-2411 <subhashu20135@iiitd.ac.in>
2023-08-01 10:18:36 +0530

    Added Load Balancing in Service Registry

--- user_service/src/main/java/com/example/user_service/service/UserServiceImplementation.java
+++ user_service/src/main/java/com/example/user_service/service/UserServiceImplementation.java
@@ -1,12 +1,19 @@
 package com.example.user_service.service;
 
 import com.example.user_service.exceptions.ResourceNotFoundException;
+import com.example.user_service.models.Hotel;
+import com.example.user_service.models.Rating;
 import com.example.user_service.models.User;
 import com.example.user_service.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.netflix.eureka.http.RestTemplateEurekaHttpClient;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImplementation implements UserService{
@@ -14,15 +21,32 @@ public class UserServiceImplementation implements UserService{
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private RestTemplate restTemplate;
+
 
     @Override
     public List<User> getUsers() {
-        return userRepository.findAll();
+        List<User> users = userRepository.findAll();
+        for(User user: users) {
+            user.setRatings(restTemplate.getForObject("http://localhost:8003/rating/user/" + user.getUserId(), ArrayList.class));
+        }
+        return users;
     }
 
     @Override
     public User getUser(String userId) {
-        return userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("Resource Not Found: " + userId));
+        User user = userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("Resource Not Found: " + userId));
+//        user.setRatings(restTemplate.getForObject("http://localhost:8003/rating/user/" + user.getUserId(), ArrayList.class).stream().map(rating -> {
+//            rating.setHotel(restTemplate.getForObject("http://localhost:8002/hotel/get/" + rating.getHotelId(), Hotel.class));
+//        }).collect(Collectors.toList()));
+        ArrayList<Rating> ratings = restTemplate.getForObject("http://RATING-SERVICE/rating/user/" + user.getUserId(), ArrayList.class);
+        ratings.stream().map(rating -> {
+            rating.setHotel(restTemplate.getForEntity("http://HOTEL_SERVICE/hotel/get/" + rating.getHotelId(), Hotel.class).getBody());
+            return rating;
+        });
+        user.setRatings(ratings);
+        return user;
     }
 
     @Override
